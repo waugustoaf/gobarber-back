62fa8bfc3eb0b7e4be07671486978c2ff2799117
waugustoaf <waltheraaf@gmail.com>
2021-04-17 14:39:42 -0300

    feat: finish phase 3
    until here, has added users creation, sessions creation and appointments creation

---
+++ src/models/Appointment.ts
@@ -0,0 +1,35 @@
+/* eslint-disable camelcase */
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    JoinColumn,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+import User from './User';
+
+@Entity('appointments')
+class Appointment {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    provider_id: string;
+
+    @ManyToOne(() => User)
+    @JoinColumn({ name: 'provider_id' })
+    provider: User;
+
+    @Column('timestamp with time zone')
+    date: Date;
+
+    @CreateDateColumn()
+    created_at: string;
+
+    @UpdateDateColumn()
+    updated_at: string;
+}
+
+export default Appointment;
---
+++ src/server.ts
@@ -0,0 +1,39 @@
+import 'reflect-metadata';
+
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import 'express-async-errors';
+
+import uploadConfig from './config/upload';
+import './database/index';
+import { AppError } from './errors/AppError';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
+app.use(routes);
+
+app.use(
+    (err: Error, request: Request, response: Response, _next: NextFunction) => {
+        if (err instanceof AppError) {
+            return response.status(err.statusCode).json({
+                status: 'error',
+                message: err.message,
+            });
+        }
+
+        // eslint-disable-next-line no-console
+        console.error(err);
+        return response
+            .status(500)
+            .json({ status: 'error', message: 'Internal server error' });
+    },
+);
+
+app.listen(3333, () => {
+    // eslint-disable-next-line no-console
+    console.log('Server started on port 3333! 💻');
+});
