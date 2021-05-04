e0bf6b918b4e0d766cca73eec75d3225bc23c62a
waltheraaf@gmail.com <waltheraaf@gmail.com>
2021-05-04 18:11:05 -0300

    test: create test and coverage  structures

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,24 @@
+import 'reflect-metadata';
+import CreateAppointmentService from './CreateAppointmentService';
+import FakeAppointmentsRepository from '../infra/typeorm/repositories/fakes/FakeAppointmentsRepository';
+
+describe('CreateAppointment', () => {
+    it('should be able to create a new appointment', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        const createAppointment = new CreateAppointmentService(
+            fakeAppointmentsRepository,
+        );
+
+        const appointment = await createAppointment.execute({
+            date: new Date(),
+            provider_id: '12345678987654321',
+        });
+
+        expect(appointment).toHaveProperty('id');
+        expect(appointment.provider_id).toBe('12345678987654321');
+    });
+
+    // it('should not be able to create two appointments on the same time', () => {
+    //     expect(1 + 2).toBe(3);
+    // });
+});
